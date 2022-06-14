1931f1aabd50d6cb8ca40dbcd6664a38d3766811
Nikita Kozlov <58469938+Mabaka@users.noreply.github.com>
2022-06-14 20:47:06 +0300

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,4 +1,4 @@
 <?php 
-    $serverName = "s01-sq03";
-    $connectionInfo = array( "Database"=>"ui", "UID"=>"sl_report", "PWD"=>"86245","CharacterSet"=>"UTF-8");
+    $serverName = "";//Данные стерты т.к. КТ.
+    $connectionInfo = array() ;
 ?>
